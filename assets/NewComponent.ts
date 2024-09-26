f94e8d053c074d668e24f9001b95a8e3a70210ea
Wen Sihao <wensihao822@gmail.com>
2024-09-27 02:07:39 +0800

    Finish hit a cuba

--- assets/NewComponent.ts
+++ assets/NewComponent.ts
@@ -1,15 +1,11 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node } from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass('NewComponent')
+@ccclass("NewComponent")
 export class NewComponent extends Component {
-    start() {
-       console.log('Hello World'); 
-    }
+  start() {
+    console.log("Hello World");
+  }
 
-    update(deltaTime: number) {
-        
-    }
+  update(deltaTime: number) {}
 }
-
-
